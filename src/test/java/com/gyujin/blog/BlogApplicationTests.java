bef587f86760b82f64f8fd7737889c28327695a2
gyujin.park <gyujin.park@baiple.com>
2023-01-20 04:32:13 +0900

    환경세티완료

---
+++ src/test/java/com/gyujin/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyujin.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
